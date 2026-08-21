3774a878dc25d1283182208d4d859498fc127bd9
myane234 <flytothemoonkawaii@gmail.com>
2026-08-21 11:09:29 +0700

    Edit seeders

--- routes/api/admin.php
+++ routes/api/admin.php
@@ -110,7 +110,9 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/admin/seeders', [AdminSeederController::class, 'index']);
     Route::post('/admin/seeders/run', [AdminSeederController::class, 'run']);
     Route::post('/admin/seeders/wilayah/run', [AdminSeederController::class, 'startWilayahImport']);
-    Route::get('/admin/seeders/wilayah/runs/{runId}', [AdminSeederController::class, 'wilayahImportStatus']);
+    Route::get('/admin/seeders/wilayah/runs/latest', [AdminSeederController::class, 'latestWilayahImportStatus']);
+    Route::get('/admin/seeders/wilayah/runs/{runId}', [AdminSeederController::class, 'wilayahImportStatus'])
+        ->whereUuid('runId');
     Route::get('/admin/seeders/wilayah-source', [AdminSeederController::class, 'wilayahSource']);
     Route::put('/admin/seeders/wilayah-source/api', [AdminSeederController::class, 'saveWilayahApi']);
     Route::post('/admin/seeders/wilayah-source/file', [AdminSeederController::class, 'saveWilayahFile']);
