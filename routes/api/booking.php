dd43062d68ad299d2d65b8702593327d160de112
myane234 <flytothemoonkawaii@gmail.com>
2026-09-05 00:21:26 +0700

    add

--- routes/api/booking.php
+++ routes/api/booking.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookingController;
 use App\Http\Controllers\WebSocketController;
 
 Route::post('/booking/charge', [BookingController::class, 'charge']);
